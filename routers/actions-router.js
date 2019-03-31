ffafe39dd8e7574b890f3f8ee3e9f34d8309d7f6
olympia <owojcik7@gmail.com>
2019-03-31 14:15:57 -0400

    Add ability to post action to actions table

--- routers/actions-router.js
+++ routers/actions-router.js
@@ -5,4 +5,25 @@ const knex = require("knex");
 const knexConfig = require("../knexfile.js");
 const db = knex(knexConfig.development);
 
+router.get("/", (req, res) => {
+  db("actions")
+    .then(actions => {
+      res.status(200).json(actions);
+    })
+    .catch(err => res.status(500).json(error));
+});
+
+router.post("/", (req, res) => {
+  db("actions")
+    .insert(req.body)
+    .then(ids => {
+      const id = ids[0];
+      db("actions")
+        .where({ action_id: id })
+        .first()
+        .then(action => res.status(201).json(action));
+    })
+    .catch(err => res.status(500).json(error));
+});
+
 module.exports = router;
